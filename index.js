c3add4d2db165125c5d1b835c7d1d42c940af2f5
chenmingming <1197729488@qq.com>
2020-06-24 13:50:44 +0800

    init

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+//分配路由
+const userRouter = require('./router/user')
+
+const app = express()
+// enable cross origin request sources
+app.use(cors())
+// parse application/json
+app.use(bodyParser.json())
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use('/user', userRouter)
+
+
+app.listen(5400, () => {
+  console.log('app is running at port 5400')
+})
